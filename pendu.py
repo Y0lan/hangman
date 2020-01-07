565ee9ab5739fcc10682b7da54a5af479bfa4013
Yolan Maldonado <work@yolan.dev>
2020-01-07 09:45:02 +0100

    test jenkins

--- pendu.py
+++ pendu.py
@@ -159,21 +159,9 @@ def main_menu_action(action_main_menu_action):
         menu(play_menu_display, play_menu_action)
 
 
-'''
-Cette fonction sert juste à aller 100 fois à la ligne pour "nettoyer l'écran" du texte affiché dessus.
-'''
-
-
 def clear():
     for i in range(0, 100):
         print("\n")
-
-
-'''
-Ces fonctions permettent de lancer un jeu soit contre un humain, soit contre la machine
-Contre la machine, il est nécessaire de choisir le niveau de difficulté.
-Contre l'humain, le niveau de difficulté est determiné par la longueur du mot entré par l'humain.
-'''
 
 
 def play_against_machine():
@@ -187,16 +175,6 @@ def play_against_human():
     play(word_picked)
 
 
-'''
-Cette fonction sert à un humain de choisir un mot que vous devrez deviner.
-En français, ligne par ligne:
-Tu rentre un mot qui sera stocké dans word_chosen
-tant que le mot choisi n'est pas dans la liste de tous les mots français du dictionnaire (donc tant que ce n'est pas 
-un mot de la langue française), rentre un autre mot
-Une fois que le mot entré est un mot de la langue française, donne le moi
-'''
-
-
 def input_word():
     word_chosen = input("Entrez un mot:\n")
     while not is_word_in_list(format_word(word_chosen)):
@@ -207,19 +185,6 @@ def input_word():
     return format_word(word_chosen)
 
 
-'''
-Cette fonction permet de savoir si le mot choisi par un humain que vous devrez deviner est dans le dictionnaire
-Pour cela la fonction va parcourir la liste de mot de la langue française ligne par ligne.
-Comme le jeu ne permet pas de minuscule ni d'accent, alors le mot entrer par le joueur est automatiquement converti
-en majuscule et les accents sont converti dans la lettre sans accent.
-
-Pour chaque ligne du fichier contenant tous les mots, mettre la ligne en majuscule sans accent et comparer le résultat
-avec le mot entrer par l'utilisateur. Si on atteint la fin de la liste de mot sans trouver de comparaison égale, le mot
-n'existe pas. Sinon, le mot existe!
-
-'''
-
-
 def is_word_in_list(word_to_verify):
     file = open("mots.txt", "r", encoding="ISO-8859-1")
     lines = file.readlines()
@@ -228,17 +193,6 @@ def is_word_in_list(word_to_verify):
         if line[0:-1] == word_to_verify:
             return True
     return False
-
-
-'''
-C'est ce qui s'affiche à l'écran quand vous jouez.
-En haut le mot PENDU.
-En dessous la difficulté du jeu
-Ensuite le pendu (si il y'a des erreurs)
-ensuite l'alphabet des lettres disponible pour jouer (celle qui n'ont jamais était joué)
-ensuite la liste de _ _ _ _ _ _, qui sont les cases vides correspondant au lettre inconnu du mot à deviner.
-ensuite, le nombre d'erreurs faite dans la partie.
-'''
 
 
 def print_game_screen(error, word, letter_already_tried, alphabet):
@@ -253,11 +207,6 @@ def print_game_screen(error, word, letter_already_tried, alphabet):
     print("ERREUR : {0}/10".format(error))
 
 
-'''
-Fonction qui selon la longueur du mot à deviner renvoie la difficulté du jeu à l'écran de jeu (fonction  juste au dessus).
-'''
-
-
 def print_difficulty(word):
     if len(word) <= 4:
         print("FACILE")
@@ -267,16 +216,6 @@ def print_difficulty(word):
         print("DIFFICILE")
     if 10 < len(word) < 100:
         print("EXPERT")
-
-
-
-'''
-Je t'ai décris toutes les fonctions sauf play().
-Avec une explication que je t'ai donné de toutes les fonctions, tu devrais être capable assez vite de comprendre
-comment fonctionne play().
-Si t'as une question bébé écris moi sur snap, je vais dodo je suis ko
-Gros bisous bon courage je crois fort en toi t'es ma numéro #1 <3
-'''
 
 
 def play(word):
@@ -337,23 +276,11 @@ def play(word):
         menu(main_menu_display, main_menu_action)
 
 
-'''
-Fonction qui parcours chaque lettre du mot à deviner et qui la compare à la lettre rentrer par l'utilisateur.
-Si l'une des lettres est la même, alors une lettre a été trouvé!
-'''
-
-
 def correct_guess(guess, word):
     for letter in word:
         if letter == guess:
             return True
     return False
-
-
-'''
-Calcule combien de lettre correct on été deviné.
-Si il y'a autant de lettre correctement deviné que de lettre dans le mots, alors le mot entier est trouvé !
-'''
 
 
 def word_is_found(letter_guessed, word_to_find):
@@ -362,11 +289,6 @@ def word_is_found(letter_guessed, word_to_find):
         if letter in letter_guessed:
             counter += 1
     return counter == len(word_to_find)
-
-
-'''
-Fonction simple qui renvoie l'alphabet en majuscule de A à Z
-'''
 
 
 def get_alphabet():
@@ -378,23 +300,11 @@ def get_alphabet():
     return alphabet
 
 
-'''
-Fonction qui affiche l'alphabet en majuscule SANS les lettres déjà devinés
-Si je tape le A dans le jeu, je n'ai pas le droit de retaper le A, alors le A ne s'affiche plus dans l'alphabet des
-lettres que je peux utiliser.
-'''
-
-
 def display_alphabet(letter_guessed, alphabet):
     for letter in alphabet:
         if letter in letter_guessed:
             alphabet.remove(letter)
     print(alphabet)
-
-
-'''
-Fonction simple permettant d'afficher les _ _ _ _ nécessaires pour remplacer les lettres du mots à deviner dans le jeu.
-'''
 
 
 def show_box_for_letter(word_to_guess, guessed_letter):
@@ -408,20 +318,6 @@ def show_box_for_letter(word_to_guess, guessed_letter):
     print(string_to_show)
 
 
-'''
-Si le niveau est FACILE, alors le mot doit faire entre 2 et 4 caractère, soit être > 1 et <= 4
-Si le niveau est INTERMEDIAIRE, alors le mot doit faire entre 5 et 6, soit être > 4 et <= 6
-Si le niveau est DIFFICILE, alors le mot doit faire entre 7 et 9, soit être > 6 et <= 10 
-...
-5, 7, 10, 100 correspondent à la limite 'inférieur à'.
-FACILE = inférieur à 5
-INTERMEDIAIRE = inférieur à 7 
-DIFFICILE = inférieur à 10
-EXPERT = inférieur à 100
-
-'''
-
-
 def get_range_for_difficulty(difficulty):
     if difficulty == 5:
         return 1, 4
@@ -431,13 +327,6 @@ def get_range_for_difficulty(difficulty):
         return 6, 9
     if difficulty == 100:
         return 9, 100
-
-
-'''
-récupère un mot au hasard de la liste tant qu'il n'est pas de la longueur adéquate supérieur à x et inférieur à y caractère
-selon à la difficulté choisie
-
-'''
 
 
 def pick_word(difficulty):
@@ -451,10 +340,6 @@ def pick_word(difficulty):
         line = format_word(line)
     return line[0:-1]
 
-
-'''
-Enlève tous les accents et met le mot en majuscule
-'''
 
 
 def format_word(word):
@@ -493,8 +378,5 @@ def uppercase(uppercase_word):
     return uppercase_word
 
 
-'''
-Là ou le programme commence. Si tu veux comprendre, suis le fil... ;)
-'''
 if __name__ == '__main__':
     menu(main_menu_display, main_menu_action)
